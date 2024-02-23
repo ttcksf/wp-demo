46d833f7d33c9beb23dfb31ad76f56aa893d6eb5
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-02-24 00:24:17 +0900

    add-404

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+    <main>
+        <!-- 1文字忘れなどは自動でリダイレクトされる -->
+        <!-- 明らかな間違いに使用され、404.phpがないとindex.phpが表示される仕組み -->
+        <section class="section contact">
+            <div class="contact_inner inner">
+                <h2 class="section_title"><span>お探しのページはありませんでした。</span></h2>
+            </div>
+        </section>
+    </main>
+<?php get_footer();?>
